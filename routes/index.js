c040d7db08aeef9fbef3c4d10c5ab88f8e16c960
ereztdev <erezt.dev@gmail.com>
2020-08-02 21:56:48 -0700

    cors.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 router.get('/api/', function(req, res, next) {
+
   setTimeout((function() { res.send(
       {
         success:true,
